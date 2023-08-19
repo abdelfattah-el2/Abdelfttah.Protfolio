ac0b1abc0e797ec6b134932bbfd344d235c1f82f
abdelfattah-el2 <abdelnasserwaled400@gmil.com>
2023-08-19 17:04:50 +0300

    new

--- src/components/Portfolio/Protfolio.js
+++ src/components/Portfolio/Protfolio.js
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 import Title from "../global/Title";
 import { PortfolioData } from "../../data";
-import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 import { Link } from "react-router-dom";
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 function Portfolio() {
   const [right, setRight] = useState(false);
   useEffect(() => {
     setRight(true);
   }, []);
- 
+
   return (
     <div className=" pb-1">
       <div
@@ -29,8 +29,7 @@ function Portfolio() {
                     className="max-w-full  dark:bg-neutral-200/50 bg-black/20   overflow-hidden p-2 "
                     key={e.id}
                   >
-             
-                    <Link to={`/Portfolio/${e.id}`} >
+                    <Link to={`/Portfolio/${e.id}`}>
                       <img
                         className=" min-w-full   max-w-full  shadow-md  dark:shadow-slate-500/20 duration-700  "
                         src={require(`../../${e.img}`)}
@@ -39,21 +38,24 @@ function Portfolio() {
                       />
                     </Link>
                     <div className="   justify-between font-medium  border  dark:bg-neutral-900/95  dark:text-white dark:border-gray-600 duration-700 border-neutral-300/90 shadow-md  dark:shadow-slate-500/20   bg-slate-50 p-2  flex   items-center">
-                    <a
-                      href={e.link}
-                      className="   font-medium    dark:bg-neutral-900/95  dark:text-white dark:border-gray-600 duration-700     bg-slate-50 p-2  flex   items-center"
-                    >
-                      <button className=" font-Mulish mr-3 ">Open <OpenInNewIcon fontSize="small" /></button>
-                    
-                    </a> 
-                     
-                    <Link className=" hover:translate-x-3 duration-700 " to={`/Portfolio/${e.id}`}>
-                      <span className="ml-2 ">
-                        <span className=" mr-1"> Details</span>
-                        <ArrowRightAltIcon  />
-                      </span>
+                      <a
+                        href={e.link}
+                        className="   font-medium    dark:bg-neutral-900/95  dark:text-white dark:border-gray-600 duration-700     bg-slate-50 p-2  flex   items-center"
+                      >
+                        <button className=" font-Mulish mr-3 ">
+                          Open <OpenInNewIcon fontSize="small" />
+                        </button>
+                      </a>
+                      <Link
+                        className=" hover:translate-x-3 duration-700 "
+                        to={`/Portfolio/${e.id}`}
+                      >
+                        <span className="ml-2 ">
+                          <span className=" mr-1"> Details</span>
+                          <ArrowRightAltIcon />
+                        </span>
                       </Link>
-                      </div>
+                    </div>
                   </div>
                 );
               })}
